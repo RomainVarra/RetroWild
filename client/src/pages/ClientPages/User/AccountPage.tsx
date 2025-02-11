674c6ec5d4c08ac8eac95b44452190355e0ef256
Romain Varra <romain.varra@hotmail.com>
2025-02-12 00:35:13 +0100

    feat: created accountPage and fix navBar style

--- client/src/pages/ClientPages/User/AccountPage.tsx
+++ client/src/pages/ClientPages/User/AccountPage.tsx
@@ -1,8 +1,34 @@
+import { useEffect, useState } from "react";
+import bobine from "../../../assets/images/bobine.jpg";
+import { useAuth } from "../../../contexts/AuthContext";
+import type { registerType } from "../../../types/user.type";
+import style from "./accountPage.module.css";
+
 function AccountPage() {
+  const { userId } = useAuth();
+  const [userAccount, setUserAccount] = useState<registerType | null>(null);
+
+  useEffect(() => {
+    if (userId) {
+      fetch(`${import.meta.env.VITE_API_URL}/api/user/${userId}`)
+        .then((response) => response.json())
+        .then((data) => setUserAccount(data))
+        .catch((error) => console.error(error));
+    }
+  }, [userId]);
   return (
-    <>
-      <h1>Mon compte</h1>
-    </>
+    <section className={style.accountSection}>
+      <h2 className={style.accountTitle}>{userAccount?.pseudo}</h2>
+      <img
+        className={style.accountPicture}
+        src={
+          userAccount?.photo
+            ? `${import.meta.env.VITE_API_URL}/uploads/${userAccount.photo}`
+            : bobine
+        }
+        alt={`illustration de ${userAccount?.pseudo}`}
+      />
+    </section>
   );
 }
 
