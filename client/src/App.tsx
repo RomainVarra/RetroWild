abe16a34c0e8b21f41017f3398050fc2fe72d5af
Romain Varra <romain.varra@hotmail.com>
2025-02-11 22:42:04 +0100

    feat: created context for userLogin

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,29 +2,32 @@ import { Outlet } from "react-router-dom";
 import "./App.css";
 import { Bounce, ToastContainer } from "react-toastify";
 import NavBar from "./components/NavBar/NavBar";
+import AuthProvider from "./contexts/AuthContext";
 
 function App() {
   return (
     <>
-      <header>
-        <NavBar />
-      </header>
-      <main>
-        <Outlet />
-        <ToastContainer
-          position="top-right"
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick={false}
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="dark"
-          transition={Bounce}
-        />
-      </main>
+      <AuthProvider>
+        <header>
+          <NavBar />
+        </header>
+        <main>
+          <Outlet />
+          <ToastContainer
+            position="top-right"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick={false}
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="dark"
+            transition={Bounce}
+          />
+        </main>
+      </AuthProvider>
     </>
   );
 }
