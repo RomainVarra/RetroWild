cf9e9013476d65e416b646301ef02f24bb40c78d
Romain Varra <romain.varra@hotmail.com>
2025-02-12 23:02:21 +0100

    feat: finish articleDetails and integration of table article to DB

--- client/src/components/Article/NewsArticlesDetails.tsx
+++ client/src/components/Article/NewsArticlesDetails.tsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import MainArticle from "../../data/MainArticle.json";
 import type { articleType } from "../../types/article.type";
+import style from "./newsArticleDetails.module.css";
 
 function NewArticlesDetails() {
   const [firstArticle, setFirstArticle] = useState<articleType | null>(null);
   const { id } = useParams();
+  const navigate = useNavigate();
 
   useEffect(() => {
     const article =
@@ -18,18 +20,38 @@ function NewArticlesDetails() {
   }
 
   return (
-    <section>
-      <h2>{firstArticle?.article_title}</h2>
-      <span>{firstArticle?.author}</span>
-      <span>{firstArticle?.published_date}</span>
-      <img
-        src={firstArticle?.picture_article}
-        alt={`Représentation de l'article ${firstArticle?.article_title}`}
-      />
-      <h3>{firstArticle?.first_title_section}</h3>
-      <p>{firstArticle?.first_section}</p>
-      <h3>{firstArticle?.second_title_section}</h3>
-      <p>{firstArticle?.second_section}</p>
+    <section className={style.articleSection}>
+      <article className={style.articleStart}>
+        <h2 className={style.articleTitle}>{firstArticle?.article_title}</h2>
+        <span className={style.articleAuthor}>
+          Auteur : {firstArticle?.author}
+        </span>
+        <span className={style.articlePublished}>
+          Publié le : {firstArticle?.published_date}
+        </span>
+        <img
+          className={style.articlePicture}
+          src={firstArticle?.picture_article}
+          alt={`Représentation de l'article ${firstArticle?.article_title}`}
+        />
+      </article>
+      <h3 className={style.articleFirstSectionTitle}>
+        {firstArticle?.first_title_section}
+      </h3>
+      <p className={style.articleFirstSection}>{firstArticle?.first_section}</p>
+      <h3 className={style.articleSecondSectionTitle}>
+        {firstArticle?.second_title_section}
+      </h3>
+      <p className={style.articleSecondSection}>
+        {firstArticle?.second_section}
+      </p>
+      <button
+        className={style.goBackButton}
+        type="button"
+        onClick={() => navigate(-1)}
+      >
+        Revenir à l'accueil
+      </button>
     </section>
   );
 }
